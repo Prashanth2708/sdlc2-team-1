2125d4f264bd3ec59c9115ba82c0e7daeb408eef
260003 <80737226+260003@users.noreply.github.com>
2021-07-06 09:13:17 +0530

    Add files via upload

---
+++ 3_Implementation/inc/color.h
@@ -0,0 +1,30 @@
+/**
+ * @file color.h
+ * @author Rahul 
+ * @brief Header file for bank manegment System
+ * @version 0.1
+ * @date 2021-04-14
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#ifndef __COLOR_H__
+#define __COLOR_H__
+
+
+
+void print_number();
+int inputVal (int *a, char b[3][10]);
+void code();
+double calResistance (int a, int b, int c);
+
+   
+
+/**
+ * @brief various functions
+
+ */
+
+
+#endif  /* __FUN_H__  */
---
+++ 3_Implementation/inc/fun.h
@@ -0,0 +1,33 @@
+/**
+ * @file fun.h
+ * @author Rahul 
+ * @brief Header file for bank manegment System
+ * @version 0.1
+ * @date 2021-04-14
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#ifndef __FUN_H__
+#define __FUN_H__
+
+
+void Switchcase();
+float Voltage(float I, float R);
+float Current(float V, float R);
+float Power(float I,float R);
+float Resistancer(float V, float I);
+float Rseries(float R1,float R2,float R3);
+float Rparllel(float R1,float R2,float R3);
+
+
+
+
+/**
+ * @brief various functions
+
+ */
+
+
+#endif  /* __FUN_H__  */
